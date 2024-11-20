38040cdcd19877ca9e65a9afd5aecd75b3d6b9fa
shaileshSwabhav <shailesh@swabhavtechlabs.com>
2024-11-20 20:58:22 +0530

    first commit

---
+++ src/app/charts/radar/radar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-radar',
+  templateUrl: './radar.component.html',
+  styleUrls: ['./radar.component.css'],
+  standalone: true,
+  imports: [CommonModule],
+})
+export class RadarComponent {
+
+}
